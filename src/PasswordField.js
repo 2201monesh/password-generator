e603cb845fb226dd926a5b498c3611b3e99addc7
Monesh Goyal <goyalmonesh2201@gmail.com>
2023-10-02 23:18:24 +0530

    Copy to clipboard

--- src/PasswordField.js
+++ src/PasswordField.js
@@ -1,10 +1,22 @@
 import { MdOutlineContentCopy } from 'react-icons/md';
+import clipboardCopy from 'clipboard-copy';
 
 function PasswordField({generatedPassword}) {
+
+   const handleCopyToClipboard = () => {
+    clipboardCopy(generatedPassword)
+      .then(() => {
+        alert('Text copied to clipboard!');
+      })
+      .catch((error) => {
+        console.error('Copy to clipboard failed:', error);
+      });
+  };
+
   return (
     <div className="inner-box-up">
       <input className="input-field" type="text" value={generatedPassword} />
-      <MdOutlineContentCopy className="icon-top" />
+      <MdOutlineContentCopy onClick={handleCopyToClipboard} className="icon-top" />
     </div>
   )
 }
